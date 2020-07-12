76b3247557b9b95c4338d41765dc861a14e999dc
Veli EROGLU <realveli24@gmail.com>
2020-07-12 18:34:48 +0300

    Check model added.

--- mainsite/models.py
+++ mainsite/models.py
@@ -120,3 +120,11 @@ class Course(ModelBase):
     def __str__(self):
         return f"{self.name}-{self.site}"
 
+
+class Check(ModelBase):
+    course = models.ForeignKey(Course, verbose_name=_("Course of Check"), on_delete=models.CASCADE)
+    start_check = models.DateTimeField(verbose_name=_("Event Start Time"), default=timezone.now)
+    end_check = models.DateTimeField(verbose_name=_("Event End Time"), default=timezone.now)
+
+    def __str__(self):
+        return f"{self.course.name}-{self.course.site}-{self.start_check}"
