eba37e4e0d9ef8065d90e7f625ba4e49c35fe20a
Veli EROGLU <realveli24@gmail.com>
2020-07-12 17:06:35 +0300

    Course module added.

--- mainsite/models.py
+++ mainsite/models.py
@@ -80,7 +80,33 @@ class User(ModelBase, AbstractUser):
                                      help_text=_("Maximum 5 MB file is allowed."))
 
     class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
         app_label = 'mainsite'
 
     def __str__(self):
         return f"{self.email}-{self.phone_number}"
+
+
+def get_sentinel_site():
+    return Site.objects.get_or_create(name='deleted')[0]
+
+
+class Course(ModelBase):
+    site = models.ForeignKey(Site, verbose_name=_("Course Site"), on_delete=models.SET(get_sentinel_site))
+    name = models.CharField(verbose_name=_("Course Name"), max_length=50, null=True, blank=False)
+    description = models.CharField(verbose_name=_("Description of Course"), max_length=5000, null=True, blank=False)
+    lecturer = models.ManyToManyField(User,
+                                       limit_choices_to={'is_staff': True},
+                                       verbose_name=_("Lecturers of Course"),
+                                       related_name='lecturer')
+    assistant = models.ManyToManyField(User,
+                                       limit_choices_to={'is_staff': True},
+                                       verbose_name=_("Assistant of Lecturer"),
+                                       related_name='assistant')
+
+    participant = models.ManyToManyField(User,
+                                          limit_choices_to={"is_staff": False},
+                                          verbose_name=_("Participants Of Course"),
+
+                                          related_name='participant')
