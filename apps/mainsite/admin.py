8c49336fed78b4bb99003c888479eb37ec3908af
Veli EROGLU <realveli24@gmail.com>
2020-07-14 14:09:39 +0300

    exportAsCSV action added to CommonAdmin

--- apps/mainsite/admin.py
+++ apps/mainsite/admin.py
@@ -1,13 +1,34 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from simple_history.admin import SimpleHistoryAdmin
 from apps.mainsite.app_models.sites import Site
 from apps.mainsite.app_models.checks import Check
 from apps.mainsite.app_models.users import User
 from apps.mainsite.app_models.courses import Course
+import csv
 
 
-class CommonAdmin(SimpleHistoryAdmin):
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
+class CommonAdmin(SimpleHistoryAdmin, ExportCsvMixin):
     empty_value_display = '-empty-'
+    actions = ['export_as_csv']
     ...
 
 
