fcf3550583a9b6a4b4149b9614bef4bd0159c456
Veli EROGLU <realveli24@gmail.com>
2020-07-14 21:35:33 +0300

    test view added.

--- backend/apps/mainsite/views.py
+++ backend/apps/mainsite/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from apps.mainsite.app_models.users import User
 
-# Create your views here.
+class Home(TemplateView):
+    template_name = 'main/main.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            **super().get_context_data(**kwargs),
+            'user': User,
+        }
